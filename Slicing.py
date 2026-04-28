f544e6befa58497ab0a546960ed51bcc30e8f306
Saketh <sakethdevulapalli@gmail.com>
2026-04-28 13:33:34 +0530

    second commit

--- Slicing.py
+++ Slicing.py
@@ -1,5 +1,5 @@
-Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
+# Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+# Enter "help" below or click "Help" above for more information.
 list_1 = [ 1, 2, 3, 4, 5 ]
 list_1
 [1, 2, 3, 4, 5]
---
+++ conditional_statements.py
@@ -0,0 +1,15 @@
+# WAP to take the age from the user and check wheather the user is adult or not. If adult, display "You are eligible for driving".
+
+'''
+
+1. Take input from the user in the from of age.
+2. If we use input only it provides the output in string format, so type cast the input function.
+3. check wheather the user is adult or not.
+4. If the user is adult, display "You are eligible for driving". 
+
+'''
+
+
+age = int(input("Enter your age : "))
+if age >= 18 :   #condition
+    print("you are eligible for driving.")  #true statement block
