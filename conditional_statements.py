7a3d57279783455689eb823a60ac39360fb9458b
Saketh <sakethdevulapalli@gmail.com>
2026-04-30 14:09:46 +0530

    Third Commit

--- conditional_statements.py
+++ conditional_statements.py
@@ -1,3 +1,5 @@
+'''  <--- IF STATEMENT --->  '''
+
 # WAP to take the age from the user and check wheather the user is adult or not. If adult, display "You are eligible for driving".
 '''
 1. Take input from the user in the from of age.
@@ -29,3 +31,62 @@ svdt = [int, float, complex, bool]
 if type(num) in svdt :
     print(True)
 '''
+
+
+
+'''  <--- IF-ELSE STATEMENT --->  '''
+
+
+#WAP to take any random character from the user and check wheather it is vowel or not. If vowel, display True. otherwise, False.
+'''
+1. Take any random character from the user.
+2. Check wheather the character is vowel or not.
+3. If vowel, print(true)
+4. If not, print(false)
+'''
+
+'''
+vowel = ['a', 'e', 'i', 'o', 'u']
+char = input('Enter your character : ')
+if char in vowel:
+    print("True")
+else :
+    print("False")
+'''
+
+#WAP to take a string from the user and check wheather it is even length string or not. If even length string then display its reverse. Otherwise, display the string as it is.
+'''
+1. Take a string as an input from the user.
+2. we have to check wheather it is even length string or not.
+3. If even, print its reverse.
+4. Otherwise, print the string as it is.
+'''
+
+'''
+word = input("Enter your random word : ")
+
+if (len(word) % 2) == 0 :
+    print(word[: : -1])
+else :
+    print(word)
+'''
+
+
+#WAP to take a number from the user and check wheather it is positive or negative. If positive, print("Positive"). Otherwise, print("Negative")
+'''
+num = int(input("Enter a random number : "))
+if num >= 0 :
+    print("Positive")
+else :
+    print("Negative")
+'''
+
+
+#WAP to take a character from the user and check wheather it is an alphabet or, a digit or, a special character.
+char = input("Enter a character : ")
+if 'A' <= char <= 'Z' or 'a' <= char <= 'z' :
+    print("alphabet")
+elif char in '0123456789' :
+    print("Digit")
+else :
+    print("Special Character")
